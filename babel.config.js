3cb35d0acdaa9358db42620de756f97ec9d70da3
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-06-24 22:44:46 +0800

    :

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports={
-	plugins: ['istanbul','canyonpw']
+	plugins: ['istanbul',
+    [
+      'canyonpw',
+      {
+        "projectID": "59119044",
+        "dsn": "https://app.canyoncov.com/coverage/client",
+        "instrumentCwd": "/app",
+        "reporter": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IndyX3poYW5nMjUiLCJpZCI6MTAxMjA1ODAsImlhdCI6MTcxODYzMzcyMiwiZXhwIjoyMDM0MjA5NzIyfQ.QBg7HOIutCR0BAaNaoS6UOxgStPGd_vuSvk5vzLS8Ss",
+        "commitSha": "58f249e185ad3c8f632ba49e8bc5bcd770b38a58"
+      }
+    ]
+  ]
 }
